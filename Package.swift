0e57adbb74f5e300b3ccccd99875acb3ee7c4805
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-09-01 13:49:30 +0200

    delete old

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Rebrickable-swift",
     platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .visionOS(.v2)],
-     dependencies: [
-         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.0"),
-         .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
-         .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.1.0"),
-     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "Rebrickable-swift",
-            dependencies: [
-                            .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                            .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-                        ],
-                        plugins: [
-                            .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
-                        ]
         ),
         .testTarget(
             name: "Rebrickable-swiftTests",
