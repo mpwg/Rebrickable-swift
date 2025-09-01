3ae9cfc2af44623b29ddf5d1ad8f54d3bf18230c
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-09-01 13:48:09 +0200

    init

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Rebrickable-swift",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Rebrickable-swift",
-            targets: ["Rebrickable-swift"]
-        ),
-    ],
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .visionOS(.v2)],
+     dependencies: [
+         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.0"),
+         .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
+         .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.1.0"),
+     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Rebrickable-swift"
+        .executableTarget(
+            name: "Rebrickable-swift",
+            dependencies: [
+                            .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                            .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+                        ],
+                        plugins: [
+                            .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+                        ]
         ),
         .testTarget(
             name: "Rebrickable-swiftTests",
