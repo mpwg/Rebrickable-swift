ca82cfcdbafe3d28e37ebce752b9edbca4b78b33
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-09-01 14:36:05 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OpenAPIClient",
-            targets: ["OpenAPIClient"]
+            name: "legoAPIClient",
+            targets: ["legoAPIClient"]
         )
     ],
     dependencies: [
@@ -25,9 +25,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OpenAPIClient",
+            name: "legoAPIClient",
             dependencies: [],
-            path: "Sources/OpenAPIClient"
+            path: "Sources/legoAPIClient"
         )
     ],
     swiftLanguageModes: [.v6]
